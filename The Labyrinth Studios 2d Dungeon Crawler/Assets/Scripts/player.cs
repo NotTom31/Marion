6f9492c31137ec3c42293c5101d90da0d4f98ce6
jsmont2 <jsaaamonteon@gmail.com>
2022-03-18 15:55:09 -0700

    added directional facing movement but still buggy atm

--- The Labyrinth Studios 2d Dungeon Crawler/Assets/Scripts/player.cs
+++ The Labyrinth Studios 2d Dungeon Crawler/Assets/Scripts/player.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class player : Mover
 {
+    public Animator animator;
+
    private void FixedUpdate()
     {
         float x = Input.GetAxisRaw("Horizontal");
         float y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", moveDelta.x);
+        animator.SetFloat("Vertical", moveDelta.y);
+        animator.SetFloat("Speed", moveDelta.sqrMagnitude);
+
+
 
         UpdateMotor(new Vector3(x, y, 0));
     }
