82f9c4146cee690d7fcd3bd4754c01944132060c
BatesThaBeast <58051984+BatesThaBeast@users.noreply.github.com>
2022-04-05 22:15:07 -0700

    Level 1 Music
    Created GameObject Level 1 Audio and added music.

---
+++ The Labyrinth Studios 2d Dungeon Crawler/Assets/Scripts/SFX.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SFX : MonoBehaviour
+{
+    public AudioSource footSteps;
+    void Update()
+    {
+        if ( Input.GetAxis("Horizontal") != 0)
+        {
+            footSteps.Play();
+        }
+        /*
+        if (Input.GetAxisRaw("Vertical") != 0)
+        {
+            footSteps.Play();
+        }
+        */
+    }
+}
