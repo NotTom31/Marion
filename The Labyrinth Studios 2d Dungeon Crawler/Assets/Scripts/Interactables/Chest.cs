7467c414809b8f61c1f759793c11331c114b98af
NotTom31 <thomasmezzie@gmail.com>
2022-05-04 15:21:19 -0700

    Started key animation

--- The Labyrinth Studios 2d Dungeon Crawler/Assets/Scripts/Interactables/Chest.cs
+++ The Labyrinth Studios 2d Dungeon Crawler/Assets/Scripts/Interactables/Chest.cs
@@ -1,17 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Chest : Collectable
 {
     public Sprite emptyChest;
     public int pesosAmount = 5;
+    public UnityEvent ChestOpened;
 
     protected override void OnCollect()
     {
         if (!collected)
         {
             collected = true;
+            ChestOpened.Invoke();
             GetComponent<SpriteRenderer>().sprite = emptyChest;
             Debug.Log("Grant " + pesosAmount + " pesos!");
         }
