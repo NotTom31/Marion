f602622e5f9d167f27fa587a572697c8e4881ee3
BatesThaBeast <58051984+BatesThaBeast@users.noreply.github.com>
2022-05-04 15:17:53 -0700

    Created EnemyMelee, EnemyRanged scripts
    Created EnemyMelee, EnemyRanged scripts. Moved code from Character and Enemy into respective enemy scripts. This is because ultimately the 2 don't share that much code and need to be different from top to bottom. Moved a few methods into player from Character as well. Hopefully this makes things a bit easier to find when others are working on the project

---
+++ The Labyrinth Studios 2d Dungeon Crawler/Assets/Scripts/Character Scripts/EnemyMelee.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMelee : Enemy, IDamageable, IKillable, IMoveable
+{
+    // Start is called before the first frame update
+    void Awake()
+    {
+        //-----------------------------
+        /*Attributes from Character script*/
+        //-----------------------------
+        moveSpeed = .014f;
+        charType = CharacterType.meleeEnemy;
+        attackDamage = 1;
+        currentHealth = 3;       
+        //-----------------------------
+        /*Attributes from Enemy script*/
+        //-----------------------------
+        target = GameObject.FindWithTag("Player").transform;//stores the player as the target
+        homePosition = this.gameObject.transform.position;//stores the home position of the enemy, this will be used to return to after chasing and for reviving
+        anim = this.GetComponent<Animator>();//Initializes the animator component
+        thisBody = this.GetComponent<Rigidbody2D>();//Initializes the Rigidbody2d component
+        chaseRadius = 1.3f;
+        attackRadius = .1f;
+        reviveRadius = 5f;
+    }
+    private void Update()
+    {
+        Move();
+    }
+    public void Move()//Author Johnathan Bates
+    {
+        Vector2 temp;//Stores the vector that will pointing towards the player
+        Vector2 tempDir;//Stores the vector that will make enemy face the player, also used in conjunction with castV3 to face home position
+        Vector2 castV3;//Stores the vector that will make enemy face it's home position
+        //this checks to see if player is in chase range but outside of "attack range", attack range is used so the enemy doesn't try to go through the player
+        if (currentState != EnemyState.dead && Vector2.Distance(transform.position, target.position) <= chaseRadius
+             && Vector2.Distance(transform.position, target.position) >= attackRadius)
+        {//moves the enemy towards the player           
+            temp = Vector2.MoveTowards(transform.position, target.position, moveSpeed);
+            thisBody.MovePosition(temp);//moves the enemy
+            currentState = EnemyState.walk;
+            tempDir = transform.position - target.position;
+            MoveInDirection(tempDir);
+        }
+        else if(currentState != EnemyState.dead)
+        {//makes the enemy return to it's home position            
+            temp = Vector2.MoveTowards(transform.position, homePosition, moveSpeed);
+            thisBody.MovePosition(temp);//moves the enemy
+            castV3 = transform.position;
+            tempDir = castV3 - homePosition;
+            MoveInDirection(tempDir);
+            currentState = EnemyState.walk;
+            if (castV3 == homePosition)
+            {
+                anim.SetBool("moving", false);
+                currentState = EnemyState.idle;
+            }
+        }
+    }
+    void MoveInDirection(Vector2 tempDir)//this will make the enemy face the direction it is moving in
+    {
+        anim.SetFloat("moveX", -tempDir.normalized.x);//allows movement animation
+        anim.SetFloat("moveY", -tempDir.normalized.y);//allows movement animation
+        anim.SetBool("moving", true);//moving set true in animator
+    }
+    //******************************************************************************************************************************************************
+    //*****************************************************************MELEE ATTACKING TRIGGERED************************************************************
+    //******************************************************************************************************************************************************
+    private void OnTriggerEnter2D(Collider2D obj)
+    {
+        if (obj.CompareTag("Player"))//check to make sure either player hits enemy or enemy hits player
+        {
+            if (obj.gameObject != null)
+            {
+                Damage(attackDamage, obj);
+                Push(obj);
+            }
+        }
+    }
+}
